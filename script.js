2f175a37a5f07e8ff5058ac5dc751cea699ce0a6
Mungai Mwaura~ <matthewmw679@gmail.com>
2024-03-08 12:26:51 +0300

    added tab redirect function for links

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+function openLink(url) {
+                // Set the location.href to the desired URL
+                location.href = "https://github.com/";
+
+                // Open the link in a new tab using window.open()
+                window.open("https://github.com/", '_blank');
+              }
